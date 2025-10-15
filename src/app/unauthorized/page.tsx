888dc207afba71aec3bbac934773697d3a47a5c6
Andrey Ramos <anrhey.capili.ramos@gmail.com>
2025-10-15 10:26:14 +0800

    refactor: fix linting issues

--- src/app/unauthorized/page.tsx
+++ src/app/unauthorized/page.tsx
@@ -2,7 +2,7 @@ export default function UnauthorizedPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-2xl font-bold">403 - Unauthorized</h1>
-      <p>You don't have permission to access this page.</p>
+      <p>You don&apos;t have permission to access this page.</p>
     </div>
   );
 }
